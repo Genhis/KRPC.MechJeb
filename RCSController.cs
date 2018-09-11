9c1881126639e744a3afb30dda672e828cbffab5
Genhis <genhis1@gmail.com>
2018-09-11 18:46:19 +0200

    Added advanced options to RCSController

--- RCSController.cs
+++ RCSController.cs
@@ -1,16 +1,25 @@
 using System.Reflection;
 
 using KRPC.Service.Attributes;
+using KRPC.SpaceCenter.ExtensionMethods;
+
+using Tuple3d = KRPC.Utils.Tuple<double, double, double>;
 
 namespace KRPC.MechJeb {
 	[KRPCClass(Service = "MechJeb")]
-	public class RCSController : ComputerModule {
+	public class RCSController : KRPCComputerModule {
 		private readonly FieldInfo rcsThrottle;
 		private readonly FieldInfo rcsForRotation;
+
+		private readonly MethodInfo setTargetRelative;
+		private readonly MethodInfo setTargetWorldVelocity;
 
 		public RCSController() : base("RCSController") {
 			this.rcsThrottle = this.type.GetField("rcsThrottle");
 			this.rcsForRotation = this.type.GetField("rcsForRotation");
+
+			this.setTargetRelative = this.type.GetMethod("SetTargetRelative");
+			this.setTargetWorldVelocity = this.type.GetMethod("SetTargetWorldVelocity");
 		}
 
 		[KRPCProperty]
@@ -23,6 +32,16 @@ namespace KRPC.MechJeb {
 		public bool RCSForRotation {
 			get => (bool)this.rcsForRotation.GetValue(this.instance);
 			set => this.rcsForRotation.SetValue(this.instance, value);
+		}
+
+		[KRPCMethod]
+		public void SetTargetRelative(Tuple3d velocity) {
+			this.setTargetRelative.Invoke(this.instance, new object[] { velocity.ToVector() });
+		}
+
+		[KRPCMethod]
+		public void SetTargetWorldVelocity(Tuple3d velocity) {
+			this.setTargetWorldVelocity.Invoke(this.instance, new object[] { velocity.ToVector() });
 		}
 	}
 }
