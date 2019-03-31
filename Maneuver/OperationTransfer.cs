d861d5a393533a7d786af0b7965587466b8010e4
Genhis <genhis1@gmail.com>
2019-03-31 17:37:43 +0200

    Replace Hohmann Transfer with Bi-Impulsive Transfers
    See MuMech/MechJeb2@de2a2e1e782a5fab1134b23184debdeab6e517b0

--- Maneuver/OperationTransfer.cs
+++ Maneuver/OperationTransfer.cs
@@ -1,11 +1,49 @@
+using System.Reflection;
+
 using KRPC.Service.Attributes;
 
 namespace KRPC.MechJeb.Maneuver {
-	/**
-	 * <summary>Hohmann transfer to target</summary>
-	 */
+	/// <summary>
+	/// Bi-impulsive (Hohmann) transfer to target
+	/// </summary>
 	[KRPCClass(Service = "MechJeb")]
-	public class OperationTransfer : Operation {
-		public OperationTransfer() : base("OperationGeneric") { }
+	public class OperationTransfer : TimedOperation {
+		private readonly FieldInfo interceptOnly;
+		private readonly object periodOffset;
+		private readonly FieldInfo simpleTransfer;
+
+		public OperationTransfer() : base("OperationGeneric") {
+			this.interceptOnly = this.type.GetField("intercept_only");
+			this.periodOffset = this.type.GetField("periodOffset").GetValue(this.instance);
+			this.simpleTransfer = this.type.GetField("simpleTransfer");
+		}
+
+		/// <summary>
+		/// Intercept only, no capture burn (impact/flyby)
+		/// </summary>
+		[KRPCProperty]
+		public bool InterceptOnly {
+			get => (bool)this.interceptOnly.GetValue(this.instance);
+			set => this.interceptOnly.SetValue(this.instance, value);
+		}
+
+		/// <summary>
+		/// Fractional target period offset
+		/// </summary>
+		[KRPCProperty]
+		public double PeriodOffset {
+			get => EditableVariables.GetDouble(this.periodOffset);
+			set => EditableVariables.SetDouble(this.periodOffset, value);
+		}
+
+		/// <summary>
+		/// Simple coplanar Hohmann transfer
+		/// </summary>
+		/// <remarks>If set to true, TimeSelector property is ignored.</remarks>
+		[KRPCProperty]
+		public bool SimpleTransfer {
+			get => (bool)this.simpleTransfer.GetValue(this.instance);
+			set => this.simpleTransfer.SetValue(this.instance, value);
+		}
 	}
 }
