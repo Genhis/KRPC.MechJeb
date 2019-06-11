b51d13a9d9201f0d6b8e8885278ba64e2de142de
Genhis <genhis1@gmail.com>
2019-06-11 11:54:51 +0200

    Updated Java examples to use AutoCloseable feature (krpc/krpc#491)

--- docs/scripts/tutorials/advanced-launch/AdvancedLaunchIntoOrbit.java
+++ docs/scripts/tutorials/advanced-launch/AdvancedLaunchIntoOrbit.java
@@ -11,42 +11,41 @@ import krpc.client.services.SpaceCenter;
 
 public class AdvancedLaunchIntoOrbit {
 	public static void main(String[] args) throws IOException, RPCException, StreamException {
-		Connection conn = Connection.newInstance("Adv. launch into orbit");
+		try(Connection conn = Connection.newInstance("Adv. launch into orbit")) {
+			SpaceCenter sc = SpaceCenter.newInstance(conn);
+			MechJeb mj = MechJeb.newInstance(conn);
+			AscentAutopilot ascent = mj.getAscentAutopilot();
 
-		SpaceCenter sc = SpaceCenter.newInstance(conn);
-		MechJeb mj = MechJeb.newInstance(conn);
-		AscentAutopilot ascent = mj.getAscentAutopilot();
+			//All of these options will be filled directly into AscentGuidance window and can be modified manually during flight.
+			ascent.setDesiredOrbitAltitude(100000);
+			ascent.setDesiredInclination(6);
 
-		//All of these options will be filled directly into AscentGuidance window and can be modified manually during flight.
-		ascent.setDesiredOrbitAltitude(100000);
-		ascent.setDesiredInclination(6);
+			ascent.setForceRoll(true);
+			ascent.setVerticalRoll(90);
+			ascent.setTurnRoll(90);
 
-		ascent.setForceRoll(true);
-		ascent.setVerticalRoll(90);
-		ascent.setTurnRoll(90);
+			ascent.setAscentPathIndex(0); //use AscentClassic as the ascent path
 
-		ascent.setAscentPathIndex(0); //use AscentClassic as the ascent path
+			AscentClassic path = ascent.getAscentPathClassic();
+			path.setTurnShapeExponent(0.5); //set the turn shape to 50%
+			path.setAutoPath(false); //don't use autopath
+			path.setTurnStartAltitude(3000);
+			path.setTurnStartVelocity(120);
+			path.setTurnEndAltitude(65000);
 
-		AscentClassic path = ascent.getAscentPathClassic();
-		path.setTurnShapeExponent(0.5); //set the turn shape to 50%
-		path.setAutoPath(false); //don't use autopath
-		path.setTurnStartAltitude(3000);
-		path.setTurnStartVelocity(120);
-		path.setTurnEndAltitude(65000);
+			ascent.setAutostage(true);
+			ascent.setEnabled(true);
+			sc.getActiveVessel().getControl().activateNextStage(); //launch the vessel
 
-		ascent.setAutostage(true);
-		ascent.setEnabled(true);
-		sc.getActiveVessel().getControl().activateNextStage(); //launch the vessel
+			Stream<Boolean> enabled = conn.addStream(ascent, "getEnabled");
+			enabled.setRate(1); //we don't need a high throughput rate, 1 second is more than enough
+			synchronized(enabled.getCondition()) {
+				while(enabled.get())
+					enabled.waitForUpdate();
+			}
+			enabled.remove();
 
-		Stream<Boolean> enabled = conn.addStream(ascent, "getEnabled");
-		enabled.setRate(1); //we don't need a high throughput rate, 1 second is more than enough
-		synchronized(enabled.getCondition()) {
-			while(enabled.get())
-				enabled.waitForUpdate();
+			System.out.println("Launch complete!");
 		}
-		enabled.remove();
-
-		System.out.println("Launch complete!");
-		conn.close();
 	}
 }
