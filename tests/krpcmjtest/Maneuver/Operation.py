651f953f80b65d00832cea36202be7fb8c5f7d44
Genhis <genhis1@gmail.com>
2020-08-24 21:46:26 +0200

    Add TimeSelector tests

--- tests/krpcmjtest/Maneuver/Operation.py
+++ tests/krpcmjtest/Maneuver/Operation.py
@@ -1,6 +1,10 @@
 from ..Annotations import InputType, Test
 from ..TestCase import TestCase
+from .TimeSelector import TimeSelectorTest
 
 class OperationTest(TestCase):
 	def __init__(self, variable, name):
 		super().__init__(variable, name)
+		self.submodules = [
+			TimeSelectorTest()
+		]
---
+++ tests/krpcmjtest/Maneuver/TimeSelector.py
@@ -0,0 +1,6 @@
+from ..Annotations import InputType, Test
+from ..TestCase import TestCase
+
+class TimeSelectorTest(TestCase):
+	def __init__(self):
+		super().__init__("time_selector", "TimeSelector")
